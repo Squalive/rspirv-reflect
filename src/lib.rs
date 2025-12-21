1c3dbf76bb56ed03c4d2183cbf384fad8a63cc24
CodingDaniel1 <cuberdanielchen@gmail.com>
2025-12-21 20:53:37 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -519,7 +519,7 @@ impl Reflection {
                 if let Some(existed) = current_set.insert(binding, descriptor_info) {
                     assert!(
                         existed.ty == ty && existed.binding_count == binding_count,
-                        "Same slot `{binding}` is bound with completely different descriptors. Existed: {:?} ({:?}), New: {:?} ({:?})",
+                        "Set({set}) Same slot({binding}) is bound with completely different descriptors. Existed: {:?} ({:?}), New: {:?} ({:?})",
                         existed.ty,
                         existed.binding_count,
                         ty,
